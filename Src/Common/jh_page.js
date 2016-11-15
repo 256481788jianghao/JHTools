4f4063cfaaeeedf5b6dea30b38f33812e99b422b
jianghao <256481788@163.com>
2016-11-15 23:03:01 +0800

    add jhPageMgr

--- Src/Common/jh_page.js
+++ Src/Common/jh_page.js
@@ -21,3 +21,42 @@ JhPage.prototype.onRelease = function(){
     return null;
 };
 
+
+var jhPageMgr = new function(){
+    var pageStack = [];
+    
+    function logE(str){
+        jhUtils.err("PageMgr:"+str);
+    }
+    function logW(str){
+        jhUtils.warn("PageMgr:"+str);
+    }
+    this.curPage = null;
+    this.lastPage = null;
+    this.registerPage = function(page){
+        if(!(page instanceof JhPage)){
+            logE("page is not a instance of JhPage");
+        }
+        logW("register Page name="+page.name);
+        pageStack.push(page);
+    };
+    this.loadPage = function(pageName){
+        logW("load page name="+pageName);
+        var isFind = false;
+        for(var i=0;i<pageStack.length;i++){
+            if(pageStack[i].name == pageName){
+                isFind = true;
+                this.lastPage = this.curPage;
+                this.curPage = pageStack[i];
+            }
+        }
+        if(!isFind){
+            logE("can not find page name="+pageName);
+        }else{
+            if(this.lastPage)
+                this.lastPage.onRelease();
+            this.curPage.onCreate();
+            this.curPage.onStart();
+        }
+    };
+}
