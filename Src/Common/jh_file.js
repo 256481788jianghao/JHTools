8f82efcc4e40b822720949ea45b14e247604cd84
jianghao <256481788@163.com>
2016-12-30 19:02:32 +0800

    add readFile and writeFile functions

--- Src/Common/jh_file.js
+++ Src/Common/jh_file.js
@@ -3,8 +3,18 @@
  */
 var jhFile = new function(){
     var self = this;
+    var fileObj = require("fs");
+    jhUtils.debug(" fileObj="+fileObj,"jhFile");
+    this.OPEN_READ = "r";
+    this.OPEN_WRITE = "w";
     this.open = function(path,mode){
-
+        return fileObj.openSync(path,mode);
+    };
+    this.readFile = function(path){
+        return fileObj.readFileSync(path,'utf8');
+    };
+    this.writeFile = function(pathi,str){
+        return fileObj.writeFileSync(path,str,'utf8');
     }
 
 };
