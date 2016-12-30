5184b8ac97d69c0d9ce3a00418385fbdbaefa950
jianghao <256481788@163.com>
2016-12-30 21:45:21 +0800

    add jh_db.js

---
+++ Src/Common/jh_db.js
@@ -0,0 +1,33 @@
+function JHDB(dbPath){
+    var m_path = dbPath;
+    var dataObj = {};
+    this.revert = function(){
+        var dataJsonStr = jhFile.readFile(m_path);
+        try{
+            dataObj = JSON.parse(dataJsonStr);
+        }catch(e){
+            dataOjb = {};
+            jhUtils.err("new JHDB str="+e.toString(),"JHDB");
+        }
+    }
+    this.getObj = function(){
+        return dataObj;
+    };
+    this.insert = function(key,value){
+        dataObj[key] = value;
+    };
+    this.getValueByKey = function(key){
+        return dataObj[key];
+    };
+    this.update = function(){
+       try{
+          jhFile.writeFile(m_path,JSON.stringify(dataObj));
+       }catch(e){
+          jhUtils.err("db update err="+e.toString(),"JHDB");
+       }
+    };
+    this.clear = function(){
+        dataObj = {};
+    };
+    this.revert();
+}
